337de6a4133da299fcb260be9079c005f4eb3bca
Jack Manoog <jack@jacks-macbook-pro-2.local>
2018-08-30 00:07:22 -0400

    Initial Commit

---
+++ FounderButtons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FounderButtons
+//
+//  Created by Jack Manoog on 8/30/18.
+//  Copyright © 2018 Manoog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
