9a1281e8499533d404d2fd5f2127a6447fcf0467
Jack Manoog <jack@jacks-macbook-pro-2.local>
2018-08-30 00:24:15 -0400

    Created version 1.1

--- FounderButtons/ViewController.swift
+++ FounderButtons/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topMessageLabel: UILabel!
+    @IBOutlet weak var bottomMessageLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-
+    @IBAction func whoFoundedGooglePressed(_ sender: UIButton) {
+        topMessageLabel.text = "Larry Page"
+        bottomMessageLabel.text = "Sergey Brin"
+    }
+    
+    @IBAction func whoFoundedRentTheRunwayPressed(_ sender: UIButton) {
+        topMessageLabel.text = "Jennifer Hyman"
+        bottomMessageLabel.text = "Jenny Fleiss"
+    }
+    
+    @IBAction func clearPressed(_ sender: UIButton) {
+        topMessageLabel.text = ""
+        bottomMessageLabel.text = ""
+        
+    }
 }
 
